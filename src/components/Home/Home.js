fd1dfd12886d95557daa8dcbb81956b02402fdce
ericgrosse <ericgrosse1@gmail.com>
2017-10-23 11:12:16 -0400

    songs now autoplay on upload

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,6 +24,7 @@ class Home extends Component {
 
       reader.onload = function(e) {
         audioArea.src = reader.result
+        audioArea.play()
       }
 
       reader.onloadstart = function() {

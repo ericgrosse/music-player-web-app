e096f1b7068aa6e4365fce145b4105b618f036e7
ericgrosse <ericgrosse1@gmail.com>
2017-11-26 20:46:20 -0500

    Prevent Heroku sleep

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,17 @@ import Header from 'components/Header/Header'
 import MobileHeader from 'components/Header/MobileHeader'
 import Routes from 'components/Routes'
 import windowSize from 'react-window-size'
+import http from 'http'
 import 'styles/app.scss'
 
 class App extends Component {
+  componentDidMount() {
+    // Ping the herokuapp url every 5 mins to prevent it from sleeping
+    setInterval(function() {
+      http.get('https://music-player-web-app.herokuapp.com/')
+    }, 300000)
+  }
+
   render() {
     const {props} = this;
 
